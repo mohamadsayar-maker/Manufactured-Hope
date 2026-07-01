e0bffd56d1c87f91b8b3e9c31a5c3df46a3289f0
Mo <mohamadsayar@gmail.com>
2026-07-01 16:52:57 +0100

    Add TraceCam service worker

---
+++ tracecam/sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'tracecam-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
